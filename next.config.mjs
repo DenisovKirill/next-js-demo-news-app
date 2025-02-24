f2326ff76bf85a26ed20d073f077da8debc698d6
Kirill Denisov <kirill.denisov.khar@gmail.com>
2025-02-24 17:17:47 +0200

    Import from root error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+    webpack(config) {
+        config.resolve.alias['@'] = resolve(__dirname);
+        return config;
+    },
+};
 
 export default nextConfig;
